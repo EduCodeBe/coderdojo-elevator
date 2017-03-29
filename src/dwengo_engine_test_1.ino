4ccbd27cd9e9c13ea78f6e081cd5e1ea94def32a
Andy Georges <itkovian@gmail.com>
2017-03-29 23:17:41 +0200

    Allow the lift to be moved using two buttons

--- src/dwengo_engine_test_1.ino
+++ src/dwengo_engine_test_1.ino
@@ -1,3 +1,4 @@
+
 #include <Arduino.h>
 
 #include <LiquidCrystal.h>
@@ -5,18 +6,49 @@
 #include <Dwenguino.h>
 #include <DwenguinoMotor.h> // the motor library
 
+// We hebben 1 motor nodig om de lift te bedienen.
+// We sluiten deze aan op de tweede poort.
 DCMotor dcMotor1(MOTOR_1_0, MOTOR_1_1);
+
+// We moeten bijhouden welke knop werd ingedrukt om te beslissen of we de lift omhoog dan wel omlaang moeten doen.
+bool is_button_pressed, button_pressed;
+
+// We stellen de bewegingssnelheid in als constante
+int LIFT_SPEED = 100;
 
 void setup() {
   initDwenguino();
+
+  is_button_pressed = false;
+  button_pressed = false;
+}
+
+void move_lift_up() {
+  dcMotor1.setSpeed(LIFT_SPEED);
+  delay(2500);
+  dcMotor1.setSpeed(0);
+}
+
+void move_lift_down() {
+  dcMotor1.setSpeed(-LIFT_SPEED);
+  delay(2500);
+  dcMotor1.setSpeed(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //dcMotor1.setSpeed(100);
-  //delay(2500);
-  //dcMotor1.setSpeed(-100);
-  //delay(2500);
 
-
+    is_button_pressed = false;
+    while(!is_button_pressed) {
+        is_button_pressed = true;
+        if(digitalRead(SW_S) == PRESSED) {
+            move_lift_down();
+        }
+        else if(digitalRead(SW_N) == PRESSED) {
+            move_lift_up();
+        }
+        else {
+            is_button_pressed = false;
+        }
+    }
+    delay(250); // wachten op de volgende druk
 }
