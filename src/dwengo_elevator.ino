676447c845bf09ef020009c2688315054a232fa5
Andy Georges <itkovian@gmail.com>
2017-05-07 13:35:44 +0200

    TEst keeping lift target

--- src/dwengo_elevator.ino
+++ src/dwengo_elevator.ino
@@ -1,4 +1,4 @@
-
+#include <math.h>
 #include <Arduino.h>
 
 #include <LiquidCrystal.h>
@@ -17,6 +17,22 @@ bool is_button_pressed, button_pressed;
 int LIFT_SPEED = 255;
 int LIFT_TIME = 200;
 
+// We houden de laatste verdieping bij waar de lift gestopt is.
+int current_lift_floor = 0;
+
+// Bepaal de tijd die nodig is om de lift 1 cm omhoog te verplaatsen met de aangegeven snelheid
+int time_required_to_move_up_one_cm(int speed) {
+    // na metingen, bekwamen de volgende formule: y = 14843 x^(-1.1225)
+    return int(14843 * pow(speed, -1.1225));
+}
+
+// Bepaal de tijd die nodig is om de lift 1 cm omlaag te verplaatsen met de aangegeven snelheid
+int time_required_to_move_down_one_cm(int speed) {
+    // na metingen, bekwamen de volgende formule: y = 5138.5 x^(0.9447)
+    return int(5138.5 * pow(speed, -0.9447));
+}
+
+
 void setup() {
   initDwenguino();
 
@@ -26,13 +42,13 @@ void setup() {
 
 void move_lift_up() {
   dcMotor1.setSpeed(LIFT_SPEED);
-  delay(LIFT_TIME);
+  delay(time_required_to_move_up_one_cm(LIFT_SPEED));
   dcMotor1.setSpeed(0);
 }
 
 void move_lift_down() {
   dcMotor1.setSpeed(-LIFT_SPEED);
-  delay(LIFT_TIME);
+  delay(time_required_to_move_down_one_cm(LIFT_SPEED));
   dcMotor1.setSpeed(0);
 }
 
@@ -43,6 +59,7 @@ void loop() {
         is_button_pressed = true;
         if(digitalRead(SW_S) == PRESSED) {
             move_lift_down();
+            
         }
         else if(digitalRead(SW_N) == PRESSED) {
             move_lift_up();
